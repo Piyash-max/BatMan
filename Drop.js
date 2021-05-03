c8100b59b7561bdb893e735f57f55946d902a4fc
Piyash-max <71182127+Piyash-max@users.noreply.github.com>
2021-05-03 17:54:17 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,27 @@
+class Drops {
+    constructor(x, y) {
+
+        var options ={
+            restitution:0.4,
+            friction:0.001
+        }
+        this.r=5;
+      
+        this.body = Bodies.circle(x, y,5,options);       
+        World.add(world, this.body);
+    }
+        updateY(){ 
+            if(this.body.position.y > height){
+                 Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400) })
+            }
+           }
+    display() {
+
+        var pos = this.body.position;
+        push();
+        ellipseMode(CENTER);
+        ellipse(pos.x, pos.y, this.r,this.r);
+        pop();
+    }
+
+};
---
+++ Umbrella.js
@@ -0,0 +1,21 @@
+class Umbrella {
+    constructor(x, y) {
+
+        var options ={
+           isStatic:true
+        }
+        this.r=50;
+        this.image=loadImage("images/Walking-Frame/walking_1.png")
+        this.body = Bodies.circle(x, y,50,options);       
+        World.add(world, this.body);
+
+    }
+    display() {
+
+        var pos = this.body.position;
+
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
+
+};
